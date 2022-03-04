37e1eea2246e828d7129f2687d0a6efdece35ff8
Christian Rieke <rieke@fh-aachen.de>
2022-03-04 16:51:39 +0100

    set date parameters to reduce test time

--- analyse.py
+++ analyse.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import sqlite3
 
-database = sqlite3.connect('/sim_result/result.db')
+database = sqlite3.connect('./sim_result/50_30/result.db')
 
 
 if __name__ == "__main__":
@@ -11,38 +11,38 @@ if __name__ == "__main__":
     df['index'] = pd.to_datetime(df['index'])
     df = df.set_index('index')
 
-    # ---> get charged power for all EVs
-    power = df['charged']
-    power.index = df.index
-    # ---> resample in 5 min and 15 min intervals
-    power_5min = power.resample('5min').mean()
-    power_15min = power.resample('15min').mean()
-    # ---> save data
-    power.to_csv('./results/charged_power_1min.csv', sep=';', decimal=',')
-    power_5min.to_csv('./results/charged_power_5min.csv', sep=';', decimal=',')
-    power_15min.to_csv('./results/charged_power_15min.csv', sep=';', decimal=',')
-
-    # ---> get requests
-    requests = df['requests']
-    requests.index = df.index
-    # ---> resample in 60 min intervals
-    requests_ = requests.resample('60min').sum()
-    requests_ = requests_.resample('5min').ffill()
-
-    requests.to_csv('./results/requests_1min.csv', sep=';', decimal=',')
-    requests_.to_csv('./results/requests_60min.csv', sep=';', decimal=',')
-
-    # ---> get commits
-    commits = df['commits']
-    commits.index = df.index
-    # ---> resample in 60 min intervals
-    commits_ = commits.resample('60min').sum()
-    commits_ = commits_.resample('5min').ffill()
-
-    commits.to_csv('./results/commits_1min.csv', sep=';', decimal=',')
-    commits_.to_csv('./results/commits_60min.csv', sep=';', decimal=',')
-
-    # ---> get soc
-    soc = df['soc']
-    soc.index = df.index
-    soc.to_csv('./results/soc.csv', sep=';', decimal=',')
+    # # ---> get charged power for all EVs
+    # power = df['charged']
+    # power.index = df.index
+    # # ---> resample in 5 min and 15 min intervals
+    # power_5min = power.resample('5min').mean()
+    # power_15min = power.resample('15min').mean()
+    # # ---> save data
+    # power.to_csv('./results/charged_power_1min.csv', sep=';', decimal=',')
+    # power_5min.to_csv('./results/charged_power_5min.csv', sep=';', decimal=',')
+    # power_15min.to_csv('./results/charged_power_15min.csv', sep=';', decimal=',')
+    #
+    # # ---> get requests
+    # requests = df['requests']
+    # requests.index = df.index
+    # # ---> resample in 60 min intervals
+    # requests_ = requests.resample('60min').sum()
+    # requests_ = requests_.resample('5min').ffill()
+    #
+    # requests.to_csv('./results/requests_1min.csv', sep=';', decimal=',')
+    # requests_.to_csv('./results/requests_60min.csv', sep=';', decimal=',')
+    #
+    # # ---> get commits
+    # commits = df['commits']
+    # commits.index = df.index
+    # # ---> resample in 60 min intervals
+    # commits_ = commits.resample('60min').sum()
+    # commits_ = commits_.resample('5min').ffill()
+    #
+    # commits.to_csv('./results/commits_1min.csv', sep=';', decimal=',')
+    # commits_.to_csv('./results/commits_60min.csv', sep=';', decimal=',')
+    #
+    # # ---> get soc
+    # soc = df['soc']
+    # soc.index = df.index
+    # soc.to_csv('./results/soc.csv', sep=';', decimal=',')
