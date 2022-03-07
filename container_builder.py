91ef38be67ba39cdfef58b94c001c7ab3a85d741
Christian Rieke <rieke@fh-aachen.de>
2022-03-07 10:49:58 +0100

    add simulation loop for each scenario

--- container_builder.py
+++ container_builder.py
@@ -9,25 +9,26 @@ ev_quotas = [50, 80, 100]
 minimum_socs = [30, 50, 80]
 
 start_date = '2022/01/01'
-end_date = '2022/03/01'
+end_date = '2022/02/01'
 
 use_london_data = False
 
 for scenario in product(ev_quotas, minimum_socs):
-    output.append(f'''
-      scenario_{scenario[0]}_{scenario[1]}:
-        container_name: s_{scenario[0]}_{scenario[1]}
-        image: {image_repo}smartdso:latest
-        build: .
-        environment:
-          EV_RATIO: {scenario[0]}
-          MINIMUM_SOC: {scenario[1]}
-          LONDON_DATA: {use_london_data}
-          START_DATE: {start_date}
-          END_DATE: {end_date}
-        volumes:
-          - ./sim_result/{scenario[0]}_{scenario[1]}:/src/sim_result
-    ''')
+    for simulation in range(5):
+        output.append(f'''
+          scenario_{scenario[0]}_{scenario[1]}_{simulation}:
+            container_name: s_{scenario[0]}_{scenario[1]}_{simulation}
+            image: {image_repo}smartdso:latest
+            build: .
+            environment:
+              EV_RATIO: {scenario[0]}
+              MINIMUM_SOC: {scenario[1]}
+              LONDON_DATA: {use_london_data}
+              START_DATE: {start_date}
+              END_DATE: {end_date}
+            volumes:
+              - ./sim_result/{scenario[0]}_{scenario[1]}/{simulation}:/src/sim_result
+        ''')
 
 with open('docker-compose.yml', 'w') as f:
     f.writelines(output)
