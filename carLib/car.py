36f5f84c7ce8b03f8b6ca426b046cd9646523121
Christian Rieke <Rieke@fh-aachen.de>
2022-03-12 12:10:31 +0100

    reformat and add comments

--- carLib/car.py
+++ carLib/car.py
@@ -1,16 +1,15 @@
-import datetime
 from datetime import datetime
-
 import pandas as pd
 import numpy as np
 
+# ---> load electric vehicle data
 electric_vehicles = pd.read_csv(r'./carLib/data/top_10_evs.csv')
 electric_vehicles['distance'] = electric_vehicles['capacity']/electric_vehicles['consumption'] * 100
 electric_vehicles['maximal_charging_power'] = 22
 
 
+# ---> function to get the EV for the corresponding distance
 def get_electric_vehicle(distance):
-
     possible_vehicles = electric_vehicles.loc[electric_vehicles['distance'] > distance]
     if len(possible_vehicles) > 0:
         probabilities = (1/possible_vehicles['weight'].sum() * possible_vehicles['weight']).values
@@ -19,8 +18,9 @@ def get_electric_vehicle(distance):
         possible_vehicles = electric_vehicles.loc[electric_vehicles['distance'] == electric_vehicles['distance'].max()]
         index = 0
 
-    return possible_vehicles.loc[index, ['model', 'capacity', 'consumption', 'distance',
-                                         'maximal_charging_power']].to_dict()
+    vehicle = possible_vehicles.loc[index, ['model', 'capacity', 'consumption',
+                                            'distance', 'maximal_charging_power']].to_dict()
+    return vehicle
 
 
 def get_fossil_vehicle():
@@ -38,29 +38,28 @@ class Car:
         else:
             properties = get_fossil_vehicle()
 
-        self.model = properties['model']
-        self.capacity = properties['capacity']
-        self.distance = round(properties['distance'], 2)
-        self.consumption = properties['consumption']
-        self.maximal_charging_power = properties['maximal_charging_power']
-        self.soc = np.random.randint(low=40, high=70)
-
-        self.total_distance = 0
-        self.demand = None
+        self.model = properties['model']                                        # ---> model type
+        self.capacity = properties['capacity']                                  # ---> capacity [kWh]
+        self.distance = round(properties['distance'], 2)                        # ---> maximal distance [km]
+        self.consumption = properties['consumption']                            # ---> consumption [kWh/100km]
+        self.maximal_charging_power = properties['maximal_charging_power']      # ---> fixed 22 [kW]
+        self.soc = np.random.randint(low=40, high=70)                           # ---> state of charge [0,..., 100]
+        self.total_distance = 0                                                 # ---> distance counter
+        self.demand = None                                                      # ---> driving demand time series
 
     def drive(self, d_time: datetime):
-        if self.demand is None:
-            self.soc -= 1
+        if self.demand is None:                                                 # ---> if no demand is st
+            self.soc -= 1                                                       # ---> decrease the soc 1 %
         else:
-            self.total_distance += self.demand[d_time] / self.consumption * 100
+            self.total_distance += self.demand[d_time] / self.consumption * 100 # ---> use time series
             energy = (self.capacity * self.soc/100) - self.demand[d_time]
             self.soc = round(energy / self.capacity * 100,2)
 
     def charge(self):
+        # ---> charge battery for 1 minute
         capacity = self.capacity * self.soc / 100 + self.maximal_charging_power/60
         self.soc = capacity / self.capacity * 100
         self.soc = min(self.soc, 100)
-
 
 if __name__ == "__main__":
     car_electric = Car()
