a0ee5fa5426779ddeed9424717193fc287525f6b
Christian Rieke <Rieke@fh-aachen.de>
2022-03-12 12:12:18 +0100

    add rejects to result_set result_set file name is correspond to the scenario name add log messages

--- main.py
+++ main.py
@@ -8,11 +8,14 @@ import logging
 import os
 from datetime import timedelta as td
 
-start_date = pd.to_datetime(os.getenv('START_DATE', '2022-02-01'))
-end_date = pd.to_datetime(os.getenv('END_DATE', '2022-02-02'))
-
 logger = logging.getLogger('Simulation')
 logger.setLevel('INFO')
+
+start_date = pd.to_datetime(os.getenv('START_DATE', '2022-02-01'))
+end_date = pd.to_datetime(os.getenv('END_DATE', '2022-02-02'))
+logger.info(f' ---> simulation for horizon {start_date.date} till {end_date.date}')
+scenario_name = os.getenv('SCENARIO_NAME', 'base_scenario')
+logger.info(f' ---> scenario {scenario_name}')
 
 logger.info(' ---> starting Flexibility Provider')
 FlexProvider = FlexibilityProvider()
@@ -43,20 +46,11 @@ database = os.getenv('DATABASE', 'result.db')
 database = sqlite3.connect(fr'./sim_result/{database}')
 database.execute('DROP TABLE IF EXISTS results')
 
+logger.info(' ---> initialize result set')
 len_ = 1440 *((end_date-start_date).days + 1)
 time_range = pd.date_range(start=start_date, periods=len_, freq='min')
-
 result = {key: pd.Series(data=np.zeros(len_), index=range(len_))
-          for key in ['commits', 'requests', 'charged', 'soc', 'price', 'ref_distance', 'ref_soc']}
-
-
-def get_data():
-    sim = sqlite3.connect('simulation.db')
-    query = 'Select t, sum(power) as power from daily_demand group by node_id, t, id_'
-    p = pd.read_sql(query, sim)
-    p = p.groupby('t').sum()
-    p = pd.Series(p.values.reshape(-1), index=range(1440))
-    return p
+          for key in ['commits', 'rejects', 'requests', 'charged', 'soc', 'price', 'ref_distance', 'ref_soc']}
 
 
 if __name__ == "__main__":
@@ -69,9 +63,9 @@ if __name__ == "__main__":
     # ---> forward the slp data to the Capacity Provider
     CapProvider.set_fixed_power(data=pd.concat(fixed_power))
 
-    indexer = 0
     # ---> start simulation for date range start_date till end_date
     logger.info(' ---> starting mobility simulation')
+    indexer = 0     # ---> minute counter for result set
     for day in pd.date_range(start=start_date, end=end_date, freq='d'):
         logger.info(f' #### simulation for day {day.date()} ####')
         # ---> build dictionary to save simulation results
@@ -85,13 +79,12 @@ if __name__ == "__main__":
                     # logger.info(f' ---> committed charging - price: {round(price,2)} ct/kWh')
                     result['commits'][indexer] += 1
                     result['price'][indexer] = price
-
                     for node_id, parameters in request.items():
                         for power, duration in parameters:
                             result['charged'][indexer:indexer + duration] += power
                             CapProvider.fixed_power[node_id][d_time:d_time + td(minutes=duration)] += power
                 else:
-                    pass
+                    result['rejects'][indexer] += 1
                     # logger.info(f' ---> rejected charging - price: {round(price,2)} ct/kWh')
             capacity = 0
             for participant in participants.values():
@@ -107,4 +100,6 @@ if __name__ == "__main__":
 
 
 result_set = pd.DataFrame(result)
-result_set.to_csv('result.csv', sep=';', decimal=',')
+result_set['price'] = result_set['price'].replace(to_replace=0, method='ffill')
+result_set.to_csv(fr'./sim_result/{scenario_name}.csv', sep=';', decimal=',')
+
