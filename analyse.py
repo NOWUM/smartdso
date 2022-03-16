d3ecc37a4461200df65c37115b8a70e11d0a7598
Christian Rieke <rieke@fh-aachen.de>
2022-03-16 16:17:50 +0100

    changed to new file output add folder for simulation and results (RXXXX, SXXXX)

--- analyse.py
+++ analyse.py
@@ -1,51 +1,40 @@
 import pandas as pd
 import numpy as np
-import sqlite3
 from glob import glob as gb
 
-path = r'./sim_result'
+path = r'./sim_result/S8030'
 
 
-def get_data(data: pd.DataFrame, type_: str):
-    return data[type_].values.flatten()
+def get_data():
+    data = []
+    files = gb(pathname=fr'{path}/*.csv')
+    for file in files:
+        if 'resample' not in file:
+            d = pd.read_csv(file, sep=';', decimal=',', index_col=0)
+            d.index = pd.to_datetime(d.index)
+            data += [d]
+    return data
 
 
-def save_data(data: list, columns: list, index: pd.Index, type_: str):
-    x = pd.DataFrame(np.asarray(data).T, columns=columns, index=[pd.to_datetime(i) for i in index])
-    if type_ in ['charged', 'soc', 'power']:
-        x = x.resample('15min').mean()
-    if type_ == 'requests' or type_ == 'commits':
-        x = x.resample('15min').sum()
-    if type_ == 'price':
-        x = x.replace(to_replace=0, method='ffill')
-        x = x.resample('15min').mean()
-
-    average = x.mean(axis=1)
-    max_ = x.max(axis=1)
-    min_ = x.min(axis=1)
-    x.loc[:, 'Average'] = average.values
-    x.loc[:, 'Maximal'] = max_.values
-    x.loc[:, 'Minimal'] = min_.values
-
-    x.to_csv(fr'{path}/{type_}.csv', sep=';', decimal=',')
+def analyse(attribute: str, dataframes: list):
+    data = np.asarray([df[attribute].values.flatten() for df in dataframes]).T
+    df = pd.DataFrame(data)
+    df['average'] = np.mean(df, axis=1)
+    df['minimum'] = np.min(df, axis=1)
+    df['maximum'] = np.max(df, axis=1)
+    df.index = dataframes[0].index
+    if parameter not in ['commits', 'requests', 'rejects']:
+        df = df.resample('15min').mean()
+    else:
+        df = df.resample('15min').sum()
+    return df
 
 
 if __name__ == "__main__":
-
-    databases = [file for file in gb(fr'{path}/*') if '.db' in file]
-    df = pd.DataFrame()
-
-    results = {type_: [] for type_ in ['power', 'charged', 'soc', 'requests', 'commits', 'price']}
-    names = []
-
-    for database in databases:
-        db = sqlite3.connect(database)
-        df = pd.read_sql('SELECT * FROM results', db)
-        df = df.set_index('index')
-        for key in results.keys():
-            results[key] += [get_data(data=df, type_=key)]
-        names.append(database.split('\\')[-1].replace('.db', ''))
-
-    for key in results.keys():
-        save_data(results[key], names, df.index, key)
+    dfs = get_data()
+    parameters = dfs[0].columns
+    results = {}
+    for parameter in parameters:
+        results[parameter] = analyse(parameter, dfs)
+        results[parameter].to_csv(fr'./sim_result/R8030/{parameter}.csv', sep=';', decimal=',')
 
