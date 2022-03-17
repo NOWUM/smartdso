4a0df18798c367e8a02ec641e742478ee4365a7b
Christian Rieke <rieke@fh-aachen.de>
2022-03-17 13:07:51 +0100

    fill templates with simulation data add csv folder for raw sim data

--- analyse.py
+++ analyse.py
@@ -1,15 +1,25 @@
 import pandas as pd
 import numpy as np
 from glob import glob as gb
+from pathlib import Path
+import os
 
-path = r'./sim_result/S8030'
+scenario = '8030'
+sim_path = fr'./sim_result/S_{scenario}'
+result_path = fr'./sim_result/R_{scenario}'
+
+if not Path(fr'{result_path}').is_dir():
+    os.mkdir(fr'{result_path}')
+if not Path(fr'{result_path}/csv'):
+    os.mkdir(fr'{result_path}/csv')
 
 
 def get_data():
     data = []
-    files = gb(pathname=fr'{path}/*.csv')
+    files = gb(pathname=fr'{sim_path}/*.csv')
     for file in files:
-        if 'resample' not in file:
+        if 'resampled' not in file and 'lmp' not in file:
+        # if 'result_1min' in file:
             d = pd.read_csv(file, sep=';', decimal=',', index_col=0)
             d.index = pd.to_datetime(d.index)
             data += [d]
@@ -19,22 +29,31 @@ def get_data():
 def analyse(attribute: str, dataframes: list):
     data = np.asarray([df[attribute].values.flatten() for df in dataframes]).T
     df = pd.DataFrame(data)
-    df['average'] = np.mean(df, axis=1)
-    df['minimum'] = np.min(df, axis=1)
-    df['maximum'] = np.max(df, axis=1)
+    df['Average'] = np.mean(df, axis=1)
+    df['Minimum'] = np.min(df, axis=1)
+    df['Maximum'] = np.max(df, axis=1)
     df.index = dataframes[0].index
-    if parameter not in ['commits', 'requests', 'rejects']:
+    if attribute not in ['commits', 'requests', 'rejects']:
         df = df.resample('15min').mean()
     else:
         df = df.resample('15min').sum()
+    columns = list(df.columns)
+    df = df.loc[:, columns[-3:] + columns[:-3]]
     return df
 
 
-if __name__ == "__main__":
+def run():
     dfs = get_data()
     parameters = dfs[0].columns
     results = {}
     for parameter in parameters:
         results[parameter] = analyse(parameter, dfs)
-        results[parameter].to_csv(fr'./sim_result/R8030/{parameter}.csv', sep=';', decimal=',')
+        results[parameter].to_csv(fr'{result_path}/csv/{parameter}.csv', sep=';', decimal=',')
+        if parameter in ['charged', 'soc', 'waiting', 'price', 'requests']:
+            with pd.ExcelWriter(fr'{result_path}/{parameter}.xlsx', if_sheet_exists='replace', mode='a') as writer:
+                results[parameter].to_excel(writer, sheet_name=parameter)
+
+
+if __name__ == "__main__":
+    run()
 
