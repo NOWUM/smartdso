52f82587039419bee540eae9c510e3048464529a
Christian Rieke <rieke@fh-aachen.de>
2022-04-22 14:45:08 +0200

    code cleaning and bug fix in group by (t) in set_fixed_power

--- agents/capacity_provider.py
+++ agents/capacity_provider.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from copy import deepcopy
 from datetime import timedelta as td, datetime
+from tqdm import tqdm
 
 from gridLib.model import GridModel
 logging.getLogger('pypsa').setLevel('ERROR')
@@ -20,45 +21,58 @@ class CapacityProvider:
                                    end=pd.to_datetime(kwargs['end_date']) + td(days=1), freq='min')[:-1]
         self.fixed_power = {i: pd.Series(index=time_range, data=np.zeros(len(time_range)), name='power')
                             for i in self.grid.data['connected'].index}
-        len_ = len(time_range)
-        self.utilization = {i: pd.Series(data=np.zeros(len_), index=time_range) for i in self.mapper.unique()}
-        self.price = {i: pd.Series(data=np.zeros(len_), index=time_range) for i in self.mapper.unique()}
-        self.congestion = {i: pd.Series(data=np.zeros(len_), index=time_range) for i in self.mapper.unique()}
+
+        self.max_transformer_capacity = dict()
+        for sub_id in self.mapper.unique():
+            transformers = self.grid.data['transformers']
+            buses_in_sub = self.grid.sub_networks[sub_id].buses.index
+            s_max = transformers.loc[transformers['bus0'].isin(buses_in_sub), 's_nom'].values[0]
+            self.max_transformer_capacity[sub_id] = s_max
+
+        data = {i: np.zeros(len(time_range)) for i in self.grid.model.lines.index}
+        self.line_utilization = pd.DataFrame(data=data, index=time_range)
+        self.line_lock = None
+
+        data = {i.replace('_slack', ''): np.zeros(len(time_range)) for i in self.grid.model.generators.index}
+        self.transformer_utilization = pd.DataFrame(data=data, index=time_range)
+        self.transformer_lock = None
 
     def set_fixed_power(self, data: pd.DataFrame):
         for index, series in self.fixed_power.items():
-            x = data.loc[data['node_id'] == index]
-            series.loc[x.index] = x['power'].to_numpy()
-            series = series.replace(to_replace=0, method='ffill')
-            self.fixed_power[index] = series
-        self._set_base_utilization()
+            power = data.loc[data['node_id'] == index].reset_index()
+            power = power.groupby('t').sum()
+            self.fixed_power[index][power.index] = power.values.flatten()
+            self.fixed_power[index] = self.fixed_power[index].replace(0, method='ffill')
 
-    def _set_base_utilization(self):
-        demand = deepcopy(self.fixed_power)
-        for sub_id in self.mapper.unique():
-            snapshots = list(pd.DataFrame(demand).drop_duplicates().index)
+        for sub_id in tqdm(self.mapper.unique()):
+            snapshots = list(pd.DataFrame(self.fixed_power).drop_duplicates().index)
             self.grid.sub_networks[sub_id].snapshots = snapshots
             for index in self.mapper[self.mapper == sub_id].index:
-                self.grid.sub_networks[sub_id].loads_t.p_set[f'{index}_consumer'] = demand[index].loc[snapshots] / 1000
+                demand = self.fixed_power[index].loc[snapshots] / 1000
+                self.grid.sub_networks[sub_id].loads_t.p_set[f'{index}_consumer'] = demand.loc[snapshots]
             self.grid.run_power_flow(sub_id=sub_id)
-            utilization = self._get_line_utilization(sub_id)
-            utilization['transformer'] = self._get_transformer_utilization(sub_id)
-            self.utilization[sub_id].loc[utilization.index] = utilization.max(axis=1)
+            self.line_lock = self._line_utilization(sub_id)
+            self.transformer_lock = self._transformer_utilization(sub_id)
+            self.set_utilization()
 
-    def _get_line_utilization(self, sub_id: str):
+    def set_utilization(self):
+        lines = deepcopy(self.line_lock)
+        self.line_utilization.loc[lines.index, lines.columns] = lines
+        transformer = deepcopy(self.transformer_lock)
+        self.transformer_utilization.loc[transformer.index, transformer.columns] = transformer.values
+
+    def _line_utilization(self, sub_id: str):
         lines = self.grid.sub_networks[sub_id].lines_t.p0
         s_max = self.grid.sub_networks[sub_id].lines.loc[:, 's_nom']
         for column in lines.columns:
             lines[column] = np.abs(lines[column]) / s_max.loc[column] * 100
         return lines
 
-    def _get_transformer_utilization(self, sub_id: str):
-        transformers = self.grid.data['transformers']
-        buses_in_sub = self.grid.sub_networks[sub_id].buses.index
-        s_max = transformers.loc[transformers['bus0'].isin(buses_in_sub), 's_nom'].values[0]
-        transformer = self.grid.sub_networks[sub_id].generators_t.p.values
-        u_tf = np.abs(transformer/s_max) * 100
-        return u_tf
+    def _transformer_utilization(self, sub_id: str):
+        transformer = self.grid.sub_networks[sub_id].generators_t.p
+        transformer.columns = [column.replace('_slack', '') for column in transformer.columns]
+        transformer = transformer/self.max_transformer_capacity[sub_id] * 100
+        return transformer
 
     def get_price(self, request: dict = None, d_time: datetime = None):
         # --> set current demand at each node
@@ -68,8 +82,7 @@ class CapacityProvider:
         for parameters in request.values():
             for power, duration in parameters:
                 demand[node_id][d_time:d_time + td(minutes=duration)] += power
-                if duration > max_duration:
-                    max_duration = duration
+                max_duration = max(duration, max_duration)
         # --> get unique timestamps
         snapshots = list(pd.DataFrame(demand).loc[d_time:d_time + td(minutes=max_duration)].drop_duplicates().index)
         # --> determine sub grid
@@ -79,20 +92,22 @@ class CapacityProvider:
             self.grid.sub_networks[sub_id].loads_t.p_set[f'{index}_consumer'] = demand[index].loc[snapshots] / 1000
         # --> run power flow calculation
         self.grid.run_power_flow(sub_id=sub_id)
-        # --> get maximal power and calculate price
-        df = self._get_line_utilization(sub_id)
-        u_tf = self._get_transformer_utilization(sub_id)
-        maximal_utilization = max(max(df.max(axis=1).to_numpy()), u_tf.max())
-        if maximal_utilization < 100:
-            price = ((-np.log(1-np.power(maximal_utilization/100, 1.5)) + 0.175) * 0.15) * 100
-            return price, maximal_utilization, sub_id
-        else:
-            self.congestion[sub_id][d_time] = 1
-            return np.inf, 100, sub_id
+        # --> get maximal utilization and calculate price
+        self.line_lock = self._line_utilization(sub_id)
+        self.transformer_lock = self._transformer_utilization(sub_id)
+        max_ = max(self.line_lock.values.max(), self.transformer_lock.values.max())
+        if max_ < 100:
+            price = ((-np.log(1-np.power(max_/100, 1.5)) + 0.175) * 0.15) * 100
+            return price, max_, sub_id
+        return np.inf, 100, sub_id
 
-    def set_charging(self, price: float, utilization: float, sub_id: str, d_time: datetime):
-        self.price[sub_id][d_time] = price
-        self.utilization[sub_id][d_time] = utilization
+    def get_results(self):
+        line_utilization = self.line_utilization.replace(0, method='ffill')
+        line_utilization = line_utilization.resample('15min').mean()
+        transformer_utilization = self.transformer_utilization.replace(0, method='ffill')
+        transformer_utilization = transformer_utilization.resample('15min').mean()
+
+        return line_utilization, transformer_utilization
 
 
 if __name__ == "__main__":
