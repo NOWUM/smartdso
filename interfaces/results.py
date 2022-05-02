29395f6aa4e6c9f3678016ff2de6fdedcbdad59d
Christian Rieke <rieke@fh-aachen.de>
2022-05-02 10:55:47 +0200

    rebuild interface as class

--- interfaces/results.py
+++ interfaces/results.py
@@ -1,159 +1,234 @@
 from sqlalchemy import create_engine, inspect
 import pandas as pd
 
-engine = create_engine('postgresql://opendata:opendata@10.13.10.41:5432/smartdso')
-tables = inspect(engine).get_table_names()
 
+class Results:
 
-def get_scenarios():
-    if len(tables) > 0:
+    def __init__(self):
+        self.engine = create_engine('postgresql://opendata:opendata@10.13.10.41:5432/smartdso')
+        self._create_tables()
+        self.tables = inspect(self.engine).get_table_names()
+        self.scenarios = self._get_scenarios()
+        if len(self.scenarios) > 0:
+            self.scenario = [*self.scenarios][0]
+            self.iterations = self._get_iterations()
+            self.iteration = [*self.iterations][0]
+        else:
+            self.scenario = None
+            self.iterations = None
+
+    def _create_tables(self):
+        # -> grid table
+        self.engine.execute("CREATE TABLE IF NOT EXISTS grid( "
+                            "time timestamp without time zone NOT NULL, "
+                            "iteration integer, "
+                            "scenario text, "
+                            "id_ text, "
+                            "sub_id integer , "
+                            "asset text, "
+                            "avg_util double precision, "
+                            "max_util double precision, "
+                            "PRIMARY KEY (time , id_, iteration, scenario));")
+
+        query_create_hypertable = "SELECT create_hypertable('grid', 'time', if_not_exists => TRUE, migrate_data => TRUE);"
+        with self.engine.connect() as connection:
+            with connection.begin():
+                connection.execute(query_create_hypertable)
+        # -> variables table
+        self.engine.execute("CREATE TABLE IF NOT EXISTS vars( "
+                            "time timestamp without time zone NOT NULL, "
+                            "iteration integer, "
+                            "scenario text, "
+                            "charged double precision, "
+                            "shifted double precision, "
+                            "price double precision, "
+                            "soc double precision, "
+                            "total_ev double precision, "
+                            "avg_distance double precision, "
+                            "avg_demand double precision, "
+                            "sub_id integer, "
+                            "PRIMARY KEY (time , iteration, scenario));")
+
+        query_create_hypertable = "SELECT create_hypertable('vars', 'time', if_not_exists => TRUE, migrate_data => TRUE);"
+        with self.engine.connect() as connection:
+            with connection.begin():
+                connection.execute(query_create_hypertable)
+        # -> car table
+        self.engine.execute("CREATE TABLE IF NOT EXISTS cars( "
+                            "time timestamp without time zone NOT NULL, "
+                            "iteration integer, "
+                            "scenario text, "
+                            "distance double precision, "
+                            "odometer double precision, "
+                            "soc double precision, "
+                            "work integer, "
+                            "errand integer, "
+                            "hobby integer, "
+                            "PRIMARY KEY (time , iteration, scenario));")
+
+        query_create_hypertable = "SELECT create_hypertable('cars', 'time', if_not_exists => TRUE, migrate_data => TRUE);"
+        with self.engine.connect() as connection:
+            with connection.begin():
+                connection.execute(query_create_hypertable)
+
+    def _get_scenarios(self):
         scenarios = dict()
-        for table in tables:
+        for table in self.tables:
             query = f'Select Distinct scenario from {table}'
-            scenarios[table] = set([value._data[0] for value in engine.execute(query).fetchall()])
-        return set.intersection(*scenarios.values())
-    else:
-        return set()
+            scenarios[table] = set([value._data[0] for value in self.engine.execute(query).fetchall()])
+        return set.intersection(*scenarios.values()) if len(scenarios) > 0 else set()
 
+    def delete_scenario(self, scenario: str):
+        for table in self.tables:
+            query = f"DELETE FROM {table} WHERE scenario='{scenario}'"
+            self.engine.execute(query)
 
-def get_iterations(scenario):
-    if len(tables) > 0:
+    def _get_iterations(self):
         iteration = dict()
-        for table in tables:
-            query = f"Select Distinct iteration from {table} where scenario='{scenario}'"
-            iteration[table] = set([value._data[0] for value in engine.execute(query).fetchall()])
-        return set.intersection(*iteration.values())
-    else:
-        return set()
+        for table in self.tables:
+            query = f"Select Distinct iteration from {table} where scenario='{self.scenario}'"
+            iteration[table] = set([value._data[0] for value in self.engine.execute(query).fetchall()])
+        return set.intersection(*iteration.values()) if len(iteration) > 0 else set()
 
+    def get_cars(self):
+        query = f"Select time, odometer, soc, work, errand, hobby from cars where scenario='{self.scenario}' " \
+                f"and iteration={self.iteration}"
+        dataframe = pd.read_sql(query, self.engine).set_index('time')
+        dataframe.index = pd.to_datetime(dataframe.index)
+        dataframe = dataframe.sort_index()
 
-def get_car_usage(scenario: str, iteration: str):
-    query = f"Select time, odometer, soc, work, errand, hobby from cars where scenario='{scenario}' " \
-            f"and iteration='{iteration}'"
-    dataframe = pd.read_sql(query, engine).set_index('time')
-    dataframe.index = pd.to_datetime(dataframe.index)
-    car_usage = dataframe.sort_index()
+        return dataframe
 
-    query = f"Select avg(evs) as evs, avg(distance) as distance, " \
-            f"avg(demand) as demand from evs where scenario='{scenario}' group by time"
-    dataframe = pd.read_sql(query, engine)
-    dataframe.index = pd.to_datetime(dataframe.index)
-    total_ev = dataframe.sort_index()
+    def get_evs(self):
+        query = f"select total_ev, avg_distance, avg_demand from vars where scenario='{self.scenario}' " \
+                f"and iteration={self.iteration}"
+        dataframe = pd.read_sql(query, self.engine)
 
-    return car_usage, total_ev
+        return dataframe.iloc[0, :]
 
+    def get_vars(self):
+        query = f"select time, avg(price) as avg_price, max(price) as max_price, min(price) as min_price, " \
+                f"avg(shifted) as avg_shifted, max(price) as max_shifted, min(shifted) as min_shifted," \
+                f"avg(charged) as charged from vars where scenario='{self.scenario}' " \
+                f"group by time order by time"
+        dataframe = pd.read_sql(query, self.engine).set_index('time')
+        dataframe.index = pd.to_datetime(dataframe.index)
+        dataframe = dataframe.resample('5min').mean()
+        for col in dataframe.columns:
+            dataframe[col] = dataframe[col].apply(lambda x: round(x, 2))
 
-def get_simulation_results(type_: str, scenario: str, iteration: str, aggregate: str = 'avg'):
-    if iteration == 'total':
-        query = f"Select time, {aggregate}({type_}) as {type_} from meta where scenario='{scenario}'" \
-                f" group by time order by time"
-    else:
-        query = f"Select time, {type_} from meta where scenario='{scenario}' and iteration={iteration} " \
-                f"order by time"
-    dataframe = pd.read_sql(query, engine).set_index('time')
-    dataframe.index = pd.to_datetime(dataframe.index)
-    dataframe = dataframe.resample('5min').mean()
-    if type_ == 'price':
-        dataframe[type_] = dataframe[type_].apply(lambda x: round(x, 2) + 29)
-    else:
-        dataframe[type_] = dataframe[type_].apply(lambda x: round(x, 2))
-    # print(dataframe.head(5))
-    return dataframe
+        return dataframe
 
+    def get_util(self, asset: str):
+        query = f"select total.id_, 100*filter.c::decimal/total.c::decimal as quantil, total.util " \
+                f"from (select id_, count(id_) as c, avg(avg_util) as util from grid " \
+                f"where scenario='{self.scenario}' and asset='{asset}' group by grid.id_) as total " \
+                f"join (select id_, count(id_) as c from grid " \
+                f"where max_util > 75 and scenario='{self.scenario}' and asset='{asset}' " \
+                f"group by grid.id_) as filter " \
+                f"on filter.id_ = total.id_ order by quantil desc limit 5"
 
-def get_transformers(scenario: str, sub_id: str):
-    # -> get the five with the highest utilization
-    if sub_id != 'total':
-        query = f"select transformers.id_, time, utilization as util from transformers " \
-                f"join (select distinct id_, max(utilization) as util from transformers where scenario='{scenario}' " \
-                f"and grid='{sub_id}'" \
-                f"group by id_ order by util desc limit 5) " \
-                f"as first5 on first5.id_=transformers.id_ and first5.util=transformers.utilization"
-    else:
-        query = f"select transformers.id_, time, utilization as util from transformers " \
-                f"join (select distinct id_, max(utilization) as util from transformers where scenario='{scenario}' " \
-                f"group by id_ order by util desc limit 5) " \
-                f"as first5 on first5.id_=transformers.id_ and first5.util=transformers.utilization"
+        dataframe = pd.read_sql(query, self.engine).set_index(['id_'])
+        dataframe = dataframe.sort_values(['util'], ascending=False)
+        dataframe.columns = [f'>75 %', 'Mean Utilization %']
+        for col in dataframe.columns:
+            dataframe[col] = dataframe[col].apply(lambda x: round(x, 2))
 
-    max_utilization = pd.read_sql(query, engine).set_index('time')
-    return max_utilization
+        return dataframe
 
+    def get_asset_util(self, id_: str):
+        query = f"select id_, max(max_util) from grid where id_='{id_}' and scenario='{self.scenario}' group by id_"
+        dataframe = pd.read_sql(query, self.engine)
+        return dataframe
 
-def get_lines(scenario: str, sub_id: str):
-    # -> get the five with the highest utilization
-    if sub_id != 'total':
-        query = f"select lines.id_, time, utilization as util from lines " \
-                f"join (select distinct id_, max(utilization) as util from lines where scenario='{scenario}' " \
-                f"and grid='{sub_id}'" \
-                f"group by id_ order by util desc limit 5) " \
-                f"as first5 on first5.id_=lines.id_ and first5.util=lines.utilization"
-    else:
-        query = f"select lines.id_, time, utilization as util from lines " \
-                f"join (select distinct id_, max(utilization) as util from lines where scenario='{scenario}' " \
-                f"group by id_ order by util desc limit 5) " \
-                f"as first5 on first5.id_=lines.id_ and first5.util=lines.utilization"
-
-    max_utilization = pd.read_sql(query, engine).set_index('time')
-
-    if sub_id != 'total':
-        query = f"Select id_, max(utilization) as utilization from lines where scenario='{scenario}' and grid='{sub_id}' " \
-                f"group by id_"
-    else:
-        query = f"Select id_, max(utilization) as utilization from lines where scenario='{scenario}' group by id_"
-
-    total_utilization = pd.read_sql(query, engine).set_index('id_')
-
-    return max_utilization, total_utilization
-
-
-def get_utilization_distribution(scenario: str, limit: int):
-
-    query = f"select total.id_, 100*filter.c::decimal/total.c::decimal as quantil, total.util " \
-            f"from (select id_, count(id_) as c, avg(utilization) as util from lines " \
-            f"where scenario='{scenario}' group by lines.id_) as total " \
-            f"join (select id_, count(id_) as c from lines " \
-            f"where utilization > {limit} and scenario='{scenario}' " \
-            f"group by lines.id_) as filter " \
-            f"on filter.id_ = total.id_ order by quantil desc limit 5"
-
-    table = pd.read_sql(query, engine).set_index(['id_'])
-    table = table.sort_values(['util'], ascending=False)
-    table.columns = [f'>{limit} %', 'Mean Utilization %']
-
-    lines = []
-    for index in table.index:
-        query = f"select id_, utilization as util from lines where id_='{index}' and scenario='{scenario}'"
-        lines += [pd.read_sql(query, engine)]
-
-    return table, pd.concat(lines)
-
-
-def get_transformer_utilization(scenario: str, sub_id: str):
-    query = f"Select time, avg(utilization) as avg, min(utilization) as min, max(utilization) as max " \
-            f"from transformers where scenario='{scenario}' and grid='{sub_id}' group by time order by time"
-
-    data = pd.read_sql(query, engine).set_index('time')
-
-    for column in data.columns:
-        data[column] = data[column].apply(lambda x: round(x, 2))
-
-    return data
-
-
-def delete_scenario(scenario: str):
-    for table in tables:
-        query = f"DELETE FROM {table} WHERE scenario='{scenario}'"
-        engine.execute(query)
+#
+# def get_transformers(scenario: str, sub_id: str):
+#     # -> get the five with the highest utilization
+#     if sub_id != 'total':
+#         query = f"select transformers.id_, time, utilization as util from transformers " \
+#                 f"join (select distinct id_, max(utilization) as util from transformers where scenario='{scenario}' " \
+#                 f"and grid='{sub_id}'" \
+#                 f"group by id_ order by util desc limit 5) " \
+#                 f"as first5 on first5.id_=transformers.id_ and first5.util=transformers.utilization"
+#     else:
+#         query = f"select transformers.id_, time, utilization as util from transformers " \
+#                 f"join (select distinct id_, max(utilization) as util from transformers where scenario='{scenario}' " \
+#                 f"group by id_ order by util desc limit 5) " \
+#                 f"as first5 on first5.id_=transformers.id_ and first5.util=transformers.utilization"
+#
+#     max_utilization = pd.read_sql(query, engine).set_index('time')
+#     return max_utilization
+#
+#
+# def get_lines(scenario: str, sub_id: str):
+#     # -> get the five with the highest utilization
+#     if sub_id != 'total':
+#         query = f"select lines.id_, time, utilization as util from lines " \
+#                 f"join (select distinct id_, max(utilization) as util from lines where scenario='{scenario}' " \
+#                 f"and grid='{sub_id}'" \
+#                 f"group by id_ order by util desc limit 5) " \
+#                 f"as first5 on first5.id_=lines.id_ and first5.util=lines.utilization"
+#     else:
+#         query = f"select lines.id_, time, utilization as util from lines " \
+#                 f"join (select distinct id_, max(utilization) as util from lines where scenario='{scenario}' " \
+#                 f"group by id_ order by util desc limit 5) " \
+#                 f"as first5 on first5.id_=lines.id_ and first5.util=lines.utilization"
+#
+#     max_utilization = pd.read_sql(query, engine).set_index('time')
+#
+#     if sub_id != 'total':
+#         query = f"Select id_, max(utilization) as utilization from lines where scenario='{scenario}' and grid='{sub_id}' " \
+#                 f"group by id_"
+#     else:
+#         query = f"Select id_, max(utilization) as utilization from lines where scenario='{scenario}' group by id_"
+#
+#     total_utilization = pd.read_sql(query, engine).set_index('id_')
+#
+#     return max_utilization, total_utilization
+#
+#
+# def get_utilization_distribution(scenario: str, limit: int):
+#     query = f"select total.id_, 100*filter.c::decimal/total.c::decimal as quantil, total.util " \
+#             f"from (select id_, count(id_) as c, avg(utilization) as util from lines " \
+#             f"where scenario='{scenario}' group by lines.id_) as total " \
+#             f"join (select id_, count(id_) as c from lines " \
+#             f"where utilization > {limit} and scenario='{scenario}' " \
+#             f"group by lines.id_) as filter " \
+#             f"on filter.id_ = total.id_ order by quantil desc limit 5"
+#
+#     table = pd.read_sql(query, engine).set_index(['id_'])
+#     table = table.sort_values(['util'], ascending=False)
+#     table.columns = [f'>{limit} %', 'Mean Utilization %']
+#
+#     return table
+#
+#
+# def get_transformer_utilization(scenario: str, sub_id: str):
+#     query = f"Select time, avg(utilization) as avg, min(utilization) as min, max(utilization) as max " \
+#             f"from transformers where scenario='{scenario}' and grid='{sub_id}' group by time order by time"
+#
+#     data = pd.read_sql(query, engine).set_index('time')
+#
+#     for column in data.columns:
+#         data[column] = data[column].apply(lambda x: round(x, 2))
+#
+#     return data
+#
+#
+#
+# def get_x(scenario: str, limit=30):
+#     query = f"select to_char(time, 'YYYY-MM-DD hh24:00:00') as t, count(utilization) " \
+#             f"from lines where scenario='{scenario}' and utilization >= {limit} group by t order by t "
+#
+#     dataframe = pd.read_sql(query, engine).set_index(['t'])
+#     dataframe.index = dataframe.index.map(pd.to_datetime)
+#     dataframe.columns = [limit]
+#     return dataframe
 
 
 if __name__ == "__main__":
-    delete_scenario(scenario='high')
-
-    # min_shifted = get_simulation_results(type_='shifted', scenario='EV100LIMIT-1', iteration='total', aggregate='min')
-    # max_shifted = get_simulation_results(type_='shifted', scenario='EV100LIMIT-1', iteration='total', aggregate='max')
-    # avg_shifted = get_simulation_results(type_='shifted', scenario='EV100LIMIT-1', iteration='total', aggregate='avg')
-    # shifted = pd.DataFrame(data=dict(min=min_shifted['shifted'].values, max=max_shifted['shifted'].values,
-    #                                  avg=avg_shifted['shifted'].values), index=avg_shifted.index)
-    # get_transformers(scenario='EV100LIMIT-1', sub_id='total')
-    # table, lines = get_utilization_distribution(scenario='EV100LIMIT-1', limit=75)
-    # car_use, evs = get_car_usage(scenario='high', iteration='high')
+    r = Results()
+    df_sim = r.get_vars()
+    df_car = r.get_cars()
+    df_util = r.get_util(asset='outlet')
+    df_evs = r.get_evs()
