a47ca0d0c5d505bb78dd2634f2d4bff49b174700
Christian Rieke <rieke@fh-aachen.de>
2022-06-10 12:02:35 +0200

    add new methods to basic class

--- participants/basic.py
+++ participants/basic.py
@@ -6,28 +6,43 @@ import pandas as pd
 
 class BasicParticipant:
 
-    def __init__(self, T: int = 96, grid_node: str = None,
-                 start_date: datetime = None, end_date: datetime = None, *args, **kwargs):
+    def __init__(self, T: int = 1440,
+                 grid_node: str = None,
+                 start_date: datetime = None, end_date: datetime = None,
+                 *args, **kwargs):
 
         # -> time resolution information
         self.T, self.t, self.dt = T, np.arange(T), 1/(T/24)
         self.time_range = pd.date_range(start=start_date, end=end_date + td(days=1), freq='min')[:-1]
         self.date_range = pd.date_range(start=start_date, end=end_date, freq='d')
-        len_ = int(len(self.time_range)/15)
         # -> input parameters and time series for the optimization
         self.weather, self.prices = pd.DataFrame(index=self.time_range), pd.DataFrame(index=self.time_range)
         # -> optimization output time series
-        # self.generation, self.demand = np.zeros(len_, float), np.zeros(len_, float)
-        self._generation = pd.Series(index=self.time_range, dtype=float)
-        self._demand = pd.Series(index=self.time_range, dtype=float)
-        self._residual_demand = pd.Series(index=self.time_range, dtype=float)
-        self._residual_generation = pd.Series(index=self.time_range, dtype=float)
+        steps = self.T * len(self.date_range)
+        self._generation = pd.Series(index=self.time_range, data=np.zeros(steps))
+        self._demand = pd.Series(index=self.time_range, data=np.zeros(steps))
+        self._residual_demand = pd.Series(index=self.time_range, data=np.zeros(steps))
+        self._residual_generation = pd.Series(index=self.time_range, data=np.zeros(steps))
+        self.power = pd.Series(index=self.time_range, data=np.zeros(steps))
         # -> grid connection node
         self.grid_node = grid_node
+        self.persons = []
 
-    def set_parameter(self, weather: pd.DataFrame = None, prices: pd.DataFrame = None):
+    def set_parameter(self, weather: pd.DataFrame = None, prices: pd.DataFrame = None) -> None:
         self.weather = weather
         self.prices = prices
 
-    def optimize(self, d_time: datetime):
+    def simulate(self, d_time: datetime) -> None:
         pass
+
+    def get_request(self, d_time: datetime) -> pd.Series:
+        self.power = pd.Series(data=np.zeros(self.T), index=pd.date_range(start=d_time, periods=self.T, freq='min'))
+        return self.power
+
+    def get_demand(self) -> (pd.Series, pd.Series):
+        return self._demand, self._residual_demand
+
+    def get_generation(self) -> (pd.Series, pd.Series):
+        return self._generation, self._residual_generation
+
+
