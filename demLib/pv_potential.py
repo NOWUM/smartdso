90e021d7ace20b549d80753995f33a1614807bc7
Christian Rieke <rieke@fh-aachen.de>
2022-06-20 15:09:40 +0200

    implemented and used shape file from solar kataster

--- demLib/pv_potential.py
+++ demLib/pv_potential.py
@@ -1,52 +1,60 @@
-from shapely import wkt
-from pyproj import Geod
 import geopandas as gpd
 import overpass as osm
 import pandas as pd
 from collections import defaultdict
-import numpy as np
+from shapely.geometry import Point
+from pyproj import Transformer
 
-# api = overpy.Overpass()
-url = "https://maps.mail.ru/osm/tools/overpass/api/interpreter"
-api = osm.API(endpoint=url, timeout=300, debug=False)
-geo_d = Geod(ellps="WGS84")
+OSM_URL = f'https://maps.mail.ru/osm/tools/overpass/api/interpreter'
+OSM_API = osm.API(endpoint=OSM_URL, timeout=300, debug=False)
+
+tr = Transformer.from_crs('EPSG:4326', 'EPSG:25832')
+
+pv_potential = gpd.read_file(r'./demLib/data/pv_potential/dueren/Solarkataster-Potentiale_05358008_Dueren_EPSG25832_Shape.shp')
 
 df = pd.read_csv(r'./gridLib/data/grid_allocations.csv', index_col=0)
-df['photovoltaic_potential'] = 0
+df['pv'] = None
 total_nodes = pd.read_csv(r'./gridLib/data/export/nodes.csv', index_col=0)
-consumer_nodes = df.loc[df['profile']=='H0','bus0']
+consumer_nodes = df.loc[df['profile'] == 'H0', 'bus0']
 nodes = total_nodes.loc[consumer_nodes]
-coords = nodes.loc[:,['lon', 'lat']]
+coords = nodes.loc[:, ['lon', 'lat']]
 coords = coords.drop_duplicates()
 
-areas = defaultdict(list)
+buildings = defaultdict(list)
 counter = 0
 for index, coord in coords.iterrows():
     query = f"way(around:{2}, {coord['lat']}, {coord['lon']})[building=yes];out qt geom;"
-    geojson = api.get(query)
+    geojson = OSM_API.get(query)
+    orientations = []
     if len(geojson['features']) > 0:
         id_ = geojson['features'][0]['id']
         data = gpd.GeoDataFrame.from_features(geojson)
-        area = abs(geo_d.geometry_area_perimeter(wkt.loads(str(data.loc[0, 'geometry'].convex_hull)))[0])
-    else:
-        id_ = counter
-        area = 50
+        x, y = data.loc[0, 'geometry'].centroid.xy
+        x, y = x[0], y[0]
+        lat, lon = tr.transform(y, x)   # -> first lat, second lon
+        # print('searching building')
+        building_id = None
+        for i, d in pv_potential.iterrows():
+            if Point(lat, lon).within(d['geometry']):
+                building_id = d['geb_id']
+                break
+        if building_id is not None:
+            for _, system in pv_potential.loc[pv_potential['geb_id'] == building_id].iterrows():
+                buildings[index] += [dict(pdc0=system['kw_17'],
+                                          surface_tilt=system['neigung'],
+                                          surface_azimuth=system['richtung'])]
 
-    areas[id_].append([index, area])
-    counter += 1
-
-total_A = 0
-for key, values in areas.items():
-    A = values[0][1]
-    total_A += A
-    l_, index = [], []
-    for c in values:
-        for i, d in df.loc[(df['bus0'] == c[0]) & (df['profile'] == 'H0'), 'jeb'].items():
-            l_ += [d]
-            index += [i]
-    pv_potential = list(A/sum(l_) * np.asarray(l_)/7)
-    for i, pv in zip(index, pv_potential):
-        df.at[i, 'photovoltaic_potential'] = pv
+for id_, systems in buildings.items():
+    consumers = df.loc[df['bus0'] == id_]
+    total_demand = consumers['jeb'].sum()
+    for index, consumer in consumers.iterrows():
+        pv_systems = []
+        for system in systems:
+            power = system['pdc0'] * consumer['jeb'] / total_demand
+            pv_systems += [dict(pdc0=round(power, 1),
+                                surface_tilt=system['surface_tilt'],
+                                surface_azimuth=system['surface_azimuth'] if system['surface_azimuth'] != -1 else 0)]
+        df.at[index, 'pv'] = pv_systems
 
 df.to_csv(r'./gridLib/data/grid_allocations.csv')
 
