23aa1dc76404b8273bbccb83f7cc87b2e40055fe
Christian Rieke <rieke@fh-aachen.de>
2022-08-18 08:05:54 +0200

    add price IT interface

--- utils.py
+++ utils.py
@@ -1,10 +1,50 @@
+import pandas as pd
 from sqlalchemy import create_engine, inspect
 import os
 
 # CREATE EXTENSION postgis;
 
 DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://opendata:opendata@10.13.10.41:5432/smartgrid')
+DATABASE_HPFC = os.getenv('DATABASE_URI', 'postgresql://opendata:opendata@10.13.10.41:5432/priceit')
 
+
+class PriceIT:
+
+    def __init__(self, table_name:str = 'price_simulation'):
+        self.engine = create_engine(DATABASE_HPFC)
+        self.price_data = pd.DataFrame()
+        self.table_name = table_name
+        self.create_table(drop=True)
+
+    def create_table(self, drop: bool = False):
+        if drop:
+            self.engine.execute(f"DROP TABLE IF EXISTS {self.table_name}")
+        self.engine.execute(f"CREATE TABLE IF NOT EXISTS {self.table_name}( "
+                            f"time timestamp without time zone NOT NULL, "
+                            f"simulation integer, "
+                            f"price double precision, "
+                            f"PRIMARY KEY (time , simulation));")
+
+        query_create_hypertable = "SELECT create_hypertable('price_simulation', 'time', if_not_exists => TRUE, migrate_data => TRUE);"
+        with self.engine.connect() as connection:
+            with connection.begin():
+                connection.execute(query_create_hypertable)
+
+    def read_csv_data(self):
+        self.price_data = pd.read_csv(r'./spotprice.csv', index_col=0, parse_dates=True)
+
+    def export_price_simulation(self):
+        if self.price_data.empty:
+            self.read_csv_data()
+
+        for col in self.price_data.columns:
+            df = self.price_data.loc[:, [col]]
+            df['simulation'] = int(col.replace('Var', ''))
+            df.columns = ['price', 'simulation']
+            df.index.name = 'time'
+            df = df[~df.index.duplicated(keep='first')]
+            df.to_sql(self.table_name, self.engine, if_exists='append')
+            print(f'imported simulation {col}')
 
 class TableCreator:
 
@@ -121,6 +161,17 @@ class TableCreator:
                             "asset text, "
                             "PRIMARY KEY (name));")
 
+    def delete_scenario(self, scenario: str):
+        for table in self.tables:
+            query = f"DELETE FROM {table} WHERE scenario='{scenario}';"
+            try:
+                self.engine.execute(query)
+            except Exception as e:
+                print(repr(e))
+
 
 if __name__ == "__main__":
-    TableCreator(create_tables=True)
+    tb = TableCreator(create_tables=False)
+    tb.delete_scenario(scenario='Simple2022')
+    # pTb = PriceIT()
+    # pTb.export_price_simulation()
