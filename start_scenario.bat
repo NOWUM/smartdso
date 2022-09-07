fd673bbf9c212d46cd0f4da32fb689013f6e1818
Christian Rieke <rieke@fh-aachen.de>
2022-09-07 10:19:48 +0200

    add strategy to start_scenario.bat

--- start_scenario.bat
+++ start_scenario.bat
@@ -1,9 +1,11 @@
 :: #!/bin/sh
-set server=10.13.10.55
+set server=10.13.10.58
 set slope=4.0
+set strategy=optimized
+set pv=80
 :: python utils.py
 :: python ./agents/capacity_provider.py
-python compose_generator.py --pv 100 --prc_sense %slope%
+python compose_generator.py --pv %pv% --prc_sense %slope% --strategy %strategy%
 ssh nowum@%server% "docker pull registry.git.fh-aachen.de/nowum-energy/projects/smartdso/smartdso:latest"
 scp .\docker-compose.yml nowum@%server%:~/smartdso/
 ssh nowum@%server% "cd smartdso && docker-compose down --remove-orphans; docker-compose up -d"
