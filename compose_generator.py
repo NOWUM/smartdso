16a457a1c72ce367433e062e554e63ce4ffa826a
Christian Rieke <rieke@fh-aachen.de>
2022-09-19 13:36:19 +0200

    new compose file to match requirements from SmartEp

--- compose_generator.py
+++ compose_generator.py
@@ -8,11 +8,16 @@ def parse_args():
     parser.add_argument('--ev', type=int, required=False, default=100, help='EV-Ratio')
     parser.add_argument('--pv', type=int, required=False, default=100, help='PV-Ratio')
     parser.add_argument('--num', type=int, required=False, default=30, help='Number of Simulations')
+    parser.add_argument('--num_offset', type=int, required=False, default=1, help='offset for simulation number (id)')
     parser.add_argument('--prc_sense', type=float, required=False, default=1.0, help='Price Sensitivity')
+    parser.add_argument('--tariff', type=str, required=False, default='const', help='Use variable or constant tariff')
     parser.add_argument('--london', type=bool, required=False, default=True, help='Use London Data')
     parser.add_argument('--start', type=str, required=False, default='2022-03-01', help='Start Date')
     parser.add_argument('--end', type=str, required=False, default='2022-03-31', help='End Date')
-    parser.add_argument('--strategy', type=str, required=False, default='optimized', help='dispatch strategy')
+    parser.add_argument('--strategy', type=str, required=False, default='PlugInCap', help='charging strategy')
+    parser.add_argument('--consumers', type=int, required=False, default=0, help='number of sample')
+    parser.add_argument('--result', type=str, required=False, default='charging', help='which results stored in db')
+
     return parser.parse_args()
 
 
@@ -21,16 +26,12 @@ if __name__ == "__main__":
 
     output = ['version: "3.9"\n', 'services:\n']
 
-    strategy = ''
+    if 'Soc' in paras.strategy:
+        strategy = f'{paras.strategy}{paras.prc_sense}'
+    else:
+        strategy = paras.strategy
 
-    if paras.strategy == 'simple':
-        strategy = 'S'
-    elif paras.strategy == 'simple_pv':
-        strategy = 'SPV'
-    elif paras.strategy == 'optimized':
-        strategy = 'O'
-
-    for simulation in range(30):
+    for simulation in range(paras.num_offset, paras.num_offset + paras.num):
         output.append(f'''
           scenario_{paras.ev}_{paras.pv}_{simulation}:
             container_name: s{paras.ev}{paras.pv}_{simulation}
@@ -45,7 +46,10 @@ if __name__ == "__main__":
               PRC_SENSE: {paras.prc_sense}
               STRATEGY: {paras.strategy}
               RANDOM_SEED: {simulation}
-              SCENARIO_NAME: EV{paras.ev}PV{paras.pv}PRC{paras.prc_sense}STR-{strategy}_{simulation}
+              RESULT_SAMPLE: {paras.result}
+              ANALYSE_GRID: "True"
+              NUMBER_CONSUMERS: {paras.consumers}
+              SCENARIO_NAME: EV{paras.ev}PV{paras.pv}PRC{paras.tariff}STR{strategy}_{simulation}
         ''')
 
     with open(f'docker-compose.yml', 'w') as f:
