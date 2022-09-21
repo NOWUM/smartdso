a99aaf2913d9cdba31a2d999ba5cabc4719b560d
Christian Rieke <rieke@fh-aachen.de>
2022-09-21 14:48:37 +0200

    add smoothing to profile and testing in main

--- demLib/heat_profile.py
+++ demLib/heat_profile.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from collections import deque
 
 REFERENCE_TEMPERATURE = np.load(r'./demLib/data/heat_demand/reference_temperature.bin')
 REFERENCE_TEMPERATURE = pd.DataFrame(data=dict(temp_air=REFERENCE_TEMPERATURE),
@@ -30,6 +31,8 @@ class StandardLoadProfile:
 
         self.kw = self.demandQ/sum(self.get_h_value())
 
+        self.temperature = deque([10, 10, 10], maxlen=3)
+
     def get_h_value(self, mean_temperature: float = None):
         if mean_temperature is None:
             return self.A / (1 + (self.B / (self.day_values - 40)) ** self.C) + self.D
@@ -40,9 +43,12 @@ class StandardLoadProfile:
         if temperature is None:
             temperature = 5*np.ones(24)
         mean_temperature = float(np.mean(temperature))
-        heat_demand_at_day = self.kw * self.get_h_value(mean_temperature)
+        t1, t2, t3 = self.temperature
+        temperature = (mean_temperature + 0.5 * t1 + 0.25 * t2 + 0.125 * t3) / (1+0.5+0.25+0.125)
+        self.temperature.appendleft(temperature)
+        heat_demand_at_day = self.kw * self.get_h_value(temperature)
 
-        hourly_heat_demand = get_hourly_factors(mean_temperature) * heat_demand_at_day
+        hourly_heat_demand = get_hourly_factors(temperature) * heat_demand_at_day
 
         if self.resolution == 96:
             return np.repeat(hourly_heat_demand, 4)
@@ -54,9 +60,15 @@ class StandardLoadProfile:
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
+    weather = pd.read_csv(r'weather.csv', index_col=0, parse_dates=True)
     heatGen = StandardLoadProfile(demandQ=40000)
+    day_range = pd.date_range(start=weather.index[0], end=weather.index[-1], freq='d')
 
-    demand = heatGen.run_model()
-
+    demand = []
+    for day in day_range:
+        demand += [heatGen.run_model(temperature=weather.loc[weather.index.date == day, 'temp_air'].values)]
+    demand = np.asarray(demand, dtype=float).flatten()
     plt.plot(demand)
+    temperature = weather['temp_air'].groupby(weather.index.day_of_year).mean().values
+    plt.plot(temperature.repeat(96))
     plt.show()
