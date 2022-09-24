bug fixes in power flow calculation add sub grid parameter to analyse a single grid add end of day method -> testing is required to check if this method must be run at days end

agents/capacity_provider.py
@@ -20,13 +20,19 @@ logger = logging.getLogger('CapacityProvider')
 class CapacityProvider:
 
     def __init__(self, scenario: str, iteration: int, start_date: datetime, end_date: datetime,
-                 database_uri: str = DATABASE_URI, T: int = 1440, write_geo: bool = True, *args, **kwargs):
+                 database_uri: str = DATABASE_URI, T: int = 1440, write_geo: bool = True,
+                 sub_grid: int = -1, *args, **kwargs):
         self.scenario = scenario
         self.iteration = iteration
         # -> build grid model and set simulation horizon
         self.grid = GridModel()
         self.mapper = self.grid.model.buses['sub_network']
         self.mapper = self.mapper[self.mapper.index.isin(self.grid.data['connected'].index)]
+        self.sub_grid = sub_grid
+        if self.sub_grid != -1:
+            self.sub_ids = [str(self.sub_grid)]
+        else:
+            self.sub_ids = self.mapper.unique()
 
         self.time_range = pd.date_range(start=start_date, end=end_date + td(days=1), freq=RESOLUTION[T])[:-1]
 
@@ -69,33 +75,38 @@ class CapacityProvider:
         transformer = transformer/s_max * 100
         return transformer
 
-    def run_power_flow(self, data: pd.DataFrame, sub_id: int) -> None:
-        demand_data = data.loc[data.index.get_level_values(0).isin(self.mapper[self.mapper == sub_id].index)]
-        demand_data = demand_data.reset_index().drop_duplicates(subset=['node_id', 'power'])
-        snapshots = list(demand_data['t'].unique())
+    def run_power_flow(self, data: pd.DataFrame, sub_id: str, end_of_day: bool = False, d_time: datetime = None) -> None:
+        nodes_in_grid = self.mapper[self.mapper.values == sub_id].index
+        demand_data = data.loc[data.index.get_level_values('node_id').isin(nodes_in_grid)]
+        demand_data = demand_data.reset_index()
+        if end_of_day:
+            snapshots = list(pd.date_range(start=d_time, periods=self.T, freq=RESOLUTION[self.T]))
+        else:
+            demand_unique = demand_data.drop_duplicates(subset=['node_id', 'power'])
+            snapshots = list(demand_unique['t'].unique())
+ 
         self.grid.sub_networks[sub_id]['model'].set_snapshots(snapshots)
         for node in demand_data['node_id'].unique():
             # -> demand time series in [kW] to [MW]
             demand = demand_data.loc[demand_data['node_id'] == node, ['t', 'power']]
             demand['power'] /= 1000
             demand = demand.set_index('t')
-            self.grid.sub_networks[sub_id]['model'].loads_t.p_set[f'{node}_consumer'] = demand
-        self.grid.sub_networks[sub_id]['model'].loads_t.p_set.fillna(method='ffill', inplace=True)
+            self.grid.sub_networks[sub_id]['model'].loads_t.p_set[f'{node}_consumer'] = demand.loc[snapshots]
         self.grid.run_power_flow(sub_id=sub_id)
 
     def set_fixed_power(self, data: pd.DataFrame) -> None:
-        data = data.groupby(['node_id', 't']).sum()
-        self.demand = data.copy()
-        for sub_id in self.mapper.unique():
-            self.run_power_flow(data=data, sub_id=sub_id)
-            snapshots = self.grid.sub_networks[sub_id]['model'].snapshots
-            self._rq_l_util = self._line_utilization(sub_id=sub_id)
-            self._rq_t_util = self._transformer_utilization(sub_id=sub_id)
+        self.demand = data.groupby(['node_id', 't']).sum()
+        for sub_id in self.sub_ids:
+            for date in np.unique(self.time_range.date):
+                self.run_power_flow(data=self.demand.copy(), sub_id=sub_id, end_of_day=True, d_time=date)
 
-            self.line_utilization[sub_id].loc[snapshots, self._rq_l_util.columns] = self._rq_l_util.values
-            self.line_utilization[sub_id].fillna(method='ffill', inplace=True)
-            self.transformer_utilization[sub_id].loc[snapshots, 'utilization'] = self._rq_t_util.values.flatten()
-            self.transformer_utilization[sub_id].fillna(method='ffill', inplace=True)
+                snapshots = self.grid.sub_networks[sub_id]['model'].snapshots
+
+                self._rq_l_util = self._line_utilization(sub_id=sub_id)
+                self._rq_t_util = self._transformer_utilization(sub_id=sub_id)
+
+                self.line_utilization[sub_id].loc[snapshots, self._rq_l_util.columns] = self._rq_l_util.values
+                self.transformer_utilization[sub_id].loc[snapshots, 'utilization'] = self._rq_t_util.values.flatten()
 
     def get_price(self, request: pd.Series = None, node_id: str = '') -> pd.Series:
         def price_func(util):
@@ -103,7 +114,6 @@ class CapacityProvider:
                 return 9_999
             else:
                 return ((-np.log(1 - np.power(util / 100, 1.5)) + 0.175) * 0.15) * 100
-
         # -> set current demand at each node
         data = self.demand.copy()
         data = data.loc[data.index.get_level_values(level='t').isin(request[request.values > 0].index)]
@@ -115,6 +125,7 @@ class CapacityProvider:
 
         data = pd.concat([data, request_], axis=0)
         data = data.groupby(['node_id', 't']).sum()
+        data = data.sort_index(level='t')
 
         sub_id = self.mapper[node_id]
         self.run_power_flow(data=data, sub_id=sub_id)
@@ -129,15 +140,13 @@ class CapacityProvider:
         return response
 
     def commit(self, request: pd.Series, node_id: str) -> None:
-        sub_id = self.mapper[node_id]
-        self.demand.loc[(self.demand.index.get_level_values(0) == node_id) &
-                        (self.demand.index.get_level_values(1).isin(request.index)), 'power'] += request.values
+        node = self.demand.index.get_level_values(0) == node_id
+        time = self.demand.index.get_level_values(1).isin(request.index)
+        self.demand.loc[node & time, 'power'] += request.values
 
-        snapshots = self.grid.sub_networks[sub_id]['model'].snapshots
-        self.line_utilization[sub_id].loc[snapshots, self._rq_l_util.columns] = self._rq_l_util.values
-        self.line_utilization[sub_id].fillna(method='ffill', inplace=True)
-        self.transformer_utilization[sub_id].loc[snapshots, 'utilization'] = self._rq_t_util.values.flatten()
-        self.transformer_utilization[sub_id].fillna(method='ffill', inplace=True)
+        # snapshots = self.grid.sub_networks[sub_id]['model'].snapshots
+        # self.line_utilization[sub_id].loc[snapshots, self._rq_l_util.columns] = self._rq_l_util.values
+        # self.transformer_utilization[sub_id].loc[snapshots, 'utilization'] = self._rq_t_util.values.flatten()
 
     def _save_summary(self, d_time: datetime) -> None:
 
@@ -203,7 +212,7 @@ class CapacityProvider:
 
         for sub_id, dataframe in self.transformer_utilization.items():
             result = dataframe.loc[time_range, ['utilization']]
-            result['id_'] = cp.grid.get_components('transformers', sub_id).name.values[0]
+            result['id_'] = self.grid.get_components('transformers', sub_id).name.values[0]
             result['asset'] = 'transformer'
             result['scenario'] = self.scenario
             result['iteration'] = self.iteration
@@ -216,7 +225,17 @@ class CapacityProvider:
             except Exception as e:
                 logger.warning(f'server closed the connection {repr(e)}')
 
+    def _run_end_of_day(self, d_time: datetime):
+        for sub_id in self.sub_ids:
+            self.run_power_flow(data=self.demand.copy(), sub_id=sub_id, end_of_day=True, d_time=d_time)
+            self._rq_l_util = self._line_utilization(sub_id=sub_id)
+            self._rq_t_util = self._transformer_utilization(sub_id=sub_id)
+            time_range = pd.date_range(start=d_time,  periods=self.T, freq=RESOLUTION[self.T])
+            self.line_utilization[sub_id].loc[time_range, self._rq_l_util.columns] = self._rq_l_util.values
+            self.transformer_utilization[sub_id].loc[time_range, 'utilization'] = self._rq_t_util.values.flatten()
+
     def save_results(self, d_time: datetime) -> None:
+        # self._run_end_of_day(d_time)
         self._save_summary(d_time)
         if self.iteration == 0:
             self._save_grid_asset(d_time)
@@ -224,5 +243,5 @@ class CapacityProvider:
 
 if __name__ == "__main__":
     cp = CapacityProvider(**dict(start_date=datetime(2022, 1, 1), end_date=datetime(2022, 1, 2),
-                                 scenario=None, iteration=None, T=96), write_geo=True)
+                                 scenario=None, iteration=None, T=96), write_geo=False)
 