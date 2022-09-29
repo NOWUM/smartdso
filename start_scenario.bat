b081536087484c0ada4b68d5e5646007819ad38e
Christian Rieke <rieke@fh-aachen.de>
2022-09-29 11:40:30 +0200

    add stack deploy

--- start_scenario.bat
+++ start_scenario.bat
@@ -1,21 +1,19 @@
 :: #!/bin/sh
-set server=10.13.10.58
+set server=10.13.10.55
 set slope=4.0
-set strategy=PlugInCap
-set pv=50
-set tariff=Flat
+set strategy=MaxPvCap
+set pv=80
+set tariff=Spot
 set consumers=0
 set num_offset=0
-set num=1
-set start=2022-03-01
-set end=2022-03-31
-:: tariff can be spot or flat
+set num=10
+set start=2022-05-01
+set end=2022-05-31
 :: python utils.py
 :: python ./agents/capacity_provider.py
-:: python utils.py
 python compose_generator.py --pv %pv% --prc_sense %slope% --strategy %strategy% --tariff %tariff% --consumers %consumers% --start %start% --end %end% --num %num% --num_offset %num_offset%
 ssh nowum@%server% "docker system prune -f"
 ssh nowum@%server% "docker pull registry.git.fh-aachen.de/nowum-energy/projects/smartdso/smartdso:latest"
 scp .\docker-compose.yml nowum@%server%:~/smartdso/
+:: ssh nowum@%server% "cd smartdso && docker stack rm smartdso; docker stack deploy --with-registry-auth -c docker-compose.yml smartdso"
 ssh nowum@%server% "cd smartdso && docker-compose down --remove-orphans; docker-compose up -d"
-ssh nowum@%server%
