fa7a6cfa7fa3de831aa439af81675a00dd1ac416
Florian Maurer <maurer@fh-aachen.de>
2022-10-06 18:29:53 +0200

    update start scenario

--- start_scenario.sh
+++ start_scenario.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-python scenario_generator.py --case b3 --slp
-
-server=10.13.10.58
-scp .\docker-compose.yml nowum@$server:~/smartdso/
-ssh nowum@$server "docker system prune -f"
-ssh nowum@$server "docker volume prune -f"
-ssh nowum@$server "docker pull registry.git.fh-aachen.de/nowum-energy/projects/smartdso/smartdso:latest"
-ssh nowum@$server "cd smartdso && docker-compose down --remove-orphans; docker-compose up -d"
+for i in "10.13.10.54 a" "10.13.10.55 b" "10.13.10.56 c" "10.13.10.58 d"; do 
+    a=( $i ); 
+    server="${a[0]}"
+    case="${a[1]}"
+    echo "$server ";
+    echo "$case";
+    
+    python scenario_generator.py --case $case
+    scp ./docker-compose.yml nowum@$server:~/smartdso/
+    ssh nowum@$server "docker system prune -f"
+    ssh nowum@$server "docker volume prune -f"
+    ssh nowum@$server "docker pull registry.git.fh-aachen.de/nowum-energy/projects/smartdso/smartdso:latest"
+    ssh nowum@$server "cd smartdso && docker-compose down --remove-orphans; docker-compose up -d"
+done
