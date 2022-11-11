a9f4390b392cf6276d90017c5a5e5af9b1659f2b
Christian Rieke <rieke@fh-aachen.de>
2022-11-11 17:14:20 +0100

    finished and tested

--- gridLib/builder.py
+++ gridLib/builder.py
@@ -1,65 +1,79 @@
 from gridLib.converter.cgmes_converter import CGMESConverter
+from gridLib.converter.dwg_converter import DWGConverter
 from gridLib.converter.pv_potential import PVConverter
 from gridLib.converter.heat_demand import HeatConverter
+from gridLib.converter.lat_lon_finder import LonLatFinder
 from gridLib.model import GridModel
 import pandas as pd
 import numpy as np
 
 LONDON_IDS = np.load(r'./gridLib/data/london_ids.npy', allow_pickle=True)
 
-PV_PATH = r"./gridLib/data/pv_potential/heinsberg/Solarkataster-Potentiale_05370016_Heinsberg_EPSG25832_Shape.shp"
-PV_CONVERTER = PVConverter(PV_PATH)
 
-HEAT_PATH = r"./gridLib/data/heat_demand/heinsberg"
-HEAT_CONVERTER = HeatConverter(HEAT_PATH)
+if __name__ == "__main__":
 
-# GRID_PATH = r"./gridLib/data/import/dem/Export.xml"
-# GRID_CONVERTER = CGMESConverter(path=GRID_PATH, levels=())
-# GRID_CONVERTER.convert()
-# CONSUMERS = GRID_CONVERTER.components["consumers"]
+    area = 'heinsberg'
 
-CONSUMERS = pd.read_csv(r'gridLib/data/export/alliander/consumers.csv', index_col=0)
+    if area == 'heinsberg':
+        pv = r"./gridLib/data/pv_potential/heinsberg/Solarkataster-Potentiale_05370016_Heinsberg_EPSG25832_Shape.shp"
+        heat = r"./gridLib/data/heat_demand/heinsberg"
+        grid = r"./gridLib/data/import/alliander/Porselen_Daten_7.dxf"
+        demand = r"./gridLib/data/import/alliander/consumer_data.xlsx"
+    else:
+        pv = r"gridLib/data/pv_potential/dueren/Solarkataster-Potentiale_05358008_Dueren_EPSG25832_Shape.shp"
+        heat = r"./gridLib/data/heat_demand/dueren"
+        grid = r"./gridLib/data/import/dem/Export.xml"
+        demand = r"./gridLib/data/import/dem/consumer_data.xlsx"
 
-# DEMAND_PATH = r"./gridLib/data/import/alliander/consumer_data.xlsx'"
-#
-# if DEMAND_PATH is not None:
-#     demand = pd.read_excel(DEMAND_PATH, sheet_name="demand")
-#     demand = demand.set_index("id_")
-#     demand.columns = ["jeb"]
-#     # -> set demand for each consumer
-#     idx = CONSUMERS['id_'].isin(demand.index)
-#     CONSUMERS = CONSUMERS.loc[idx]
-#     CONSUMERS = CONSUMERS.join(demand, on="id_")
-#     CONSUMERS = CONSUMERS.dropna()
-# else:
-#     CONSUMERS['jeb'] = 4500
+    if 'dxf' in grid:
+        grid_conv = DWGConverter(grid)
+    else:
+        grid_conv = CGMESConverter(grid)
 
-CONSUMERS.rename(columns={'jeb': 'demand_power'}, inplace=True)
-CONSUMERS['pv'] = None
-CONSUMERS['demand_heat'] = 10_000
+    grid_conv.convert()
 
-#CONSUMERS['london_data'] = [np.random.choice(LONDON_IDS) for _ in range(len(CONSUMERS))]
-#CONSUMERS['pv'] = PV_CONVERTER.add_pv_to_consumers(CONSUMERS.copy())
+    model = GridModel(
+        nodes=grid_conv.components['nodes'],
+        lines=grid_conv.components['lines'],
+        transformers=grid_conv.components['transformers'],
+        consumers=grid_conv.components['consumers']
+    )
 
-#years, demand = HEAT_CONVERTER.add_heat_demand_to_consumers(CONSUMERS.copy())
-#CONSUMERS['year'] = years
-#CONSUMERS['demand_heat'] = demand
+    def get_sub_grid(consumer_node):
+        return int(model.model.buses.loc[consumer_node, 'sub_network'])
+    consumers = grid_conv.components['consumers']
+    consumers['sub_grid'] = consumers['bus0'].apply(get_sub_grid)
 
-nodes = pd.read_csv(r'gridLib/data/export/alliander/nodes.csv', index_col=0)
-lines = pd.read_csv(r'gridLib/data/export/alliander/lines.csv', index_col=0)
-transformers = pd.read_csv(r'gridLib/data/export/alliander/transformers.csv', index_col=0)
-MODEL = GridModel(
-    nodes=nodes,
-    lines=lines,
-    transformers=transformers,
-    consumers=CONSUMERS
-)
+    if demand is not None:
+        demand = pd.read_excel(demand, sheet_name="demand")
+        demand = demand.set_index("id_")
+        # -> set demand for each consumer
+        idx = consumers.index.isin(demand.index)
+        if any(idx):
+            consumers = consumers.loc[idx]
+            consumers = consumers.join(demand, on="id_")
+            consumers = consumers.dropna()
+        else:
+            consumers['profile'] = "H0"
+            finder = LonLatFinder(demand)
+            finder.find_coords()
+            consumers = finder.map_to_grid(consumers)
+    else:
+        consumers['jeb'] = 4500
 
+    consumers.rename(columns={'jeb': 'demand_power'}, inplace=True)
 
-def get_sub_grid(consumer_node):
-    return int(MODEL.model.buses.loc[consumer_node, 'sub_network'])
+    pv_converter = PVConverter(pv)
+    pv_consumers = pv_converter.add_pv_to_consumers(consumers.copy())
 
+    for idx, data in pv_consumers.items():
+        consumers.at[idx, 'pv'] = data
 
-CONSUMERS['sub_grid'] = CONSUMERS['bus0'].apply(get_sub_grid)
-#
-# GRID_CONVERTER.save(r"./gridLib/data/export/dem")
+    heat_converter = HeatConverter(heat)
+    years, demand = heat_converter.add_heat_demand_to_consumers(consumers.copy())
+    consumers['year'] = years
+    consumers['demand_heat'] = demand
+
+    consumers['london_data'] = np.random.choice(LONDON_IDS, size=len(consumers))
+
+    grid_conv.save(r"./gridLib/data/export/alliander/")
