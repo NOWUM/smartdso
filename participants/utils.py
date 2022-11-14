72adf0794c4642d36bc4e5f07b7cd3bd776a05fd
Christian Rieke <rieke@fh-aachen.de>
2022-11-14 14:14:58 +0100

    add literature value for hot water volume

--- participants/utils.py
+++ participants/utils.py
@@ -1,6 +1,7 @@
 
 class HeatStorage:
-
-    def __init__(self, volume: float, dtheta: int):
-        self.volume = (volume * 0.997 * 4.19 * dtheta) / 3600
+    # -> 30 bis 48 Liter (BDEW) hot water per day ~ 50 liter/person and day
+    def __init__(self, volume: float, d_theta: int):
+        self.volume = (volume * 0.997 * 4.19 * d_theta) / 3600
         self.loss = 10.5 * volume / 3600
+        self.V0 = 0
