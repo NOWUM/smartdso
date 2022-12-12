d5b4c179e989d0769cdf35b3f56affadb8d9d5b9
Christian Rieke <rieke@fh-aachen.de>
2022-12-12 08:49:50 +0100

    add losses to get analyze distribution

--- agents/energy_provider.py
+++ agents/energy_provider.py
@@ -17,14 +17,16 @@ class EnergyProvider:
         self.ts = len(self.charging_data[0])
         self.price_data = pickle.load(open(r'./agents/data/price_data.pkl', 'rb'))
 
-        self._tm_price = np.asarray(list(self.price_data.values())).mean()
-        self._da_price = np.asarray(list(self.price_data.values())).mean(axis=0)
+        self._tm_price = np.asarray(list(self.price_data.values())).mean() / 1e3
+        self._da_price = np.asarray(list(self.price_data.values())).mean(axis=0) / 1e3
 
         self.portfolio_size = 50
         self.samples = 30
 
         self._portfolios = []
         self._prices = []
+
+        self._losses = []
 
         self.results = {'losses': [],
                         'tariff': []}
@@ -49,7 +51,7 @@ class EnergyProvider:
 
     def optimize(self, beta: float = 0.95, r_interest: float = 0.2, price_fixed: bool = True):
         m = ConcreteModel()
-        solver = SolverFactory('glpk')
+        solver = SolverFactory('gurobi')
 
         samples = [i for i in range(self.samples)]
         timesteps = [i for i in range(self.ts)]
@@ -103,6 +105,8 @@ class EnergyProvider:
 
         self.results['losses'] = [value(loss[s]) for s in samples]
 
+        self._losses = [value(l) for l in loss]
+
         print(value(m.obj))
 
         return m
@@ -112,21 +116,40 @@ if __name__ == "__main__":
 
     from matplotlib import pyplot as plt
     from matplotlib.ticker import PercentFormatter
-
+    from tqdm import tqdm
     fig, ax = plt.subplots(5, 1, tight_layout=True)
-    kwargs = dict(histtype='stepfilled', alpha=1.0, bins=100, range=(0, 8))
+    # kwargs = dict(histtype='stepfilled', alpha=1.0, bins=100, range=(0, 8))
     ep = EnergyProvider()
-    for k, row in zip([10, 100, 200, 500, 1000], range(5)):
-        print(k)
-        ep.set_portfolio(k, 200)
-        x = np.asarray(ep._portfolios)
-        n, bins, patches = ax[row].hist(x.flatten(), **kwargs)
-        ax[row].yaxis.set_major_formatter(PercentFormatter(xmax=len(x.flatten() / n.max())))
-        ax[row].legend([k])
-        ax[row].grid(True)
-        ax[row].set_ylim([0, len(x.flatten()) * 0.20])
+    ep.set_portfolio(30, 100)
+    ep.optimize(r_interest=0.0)
+    plt.hist(ep._losses, bins=30)
+    ep.optimize(r_interest=0.1)
+    plt.hist(ep._losses, bins=30)
 
     plt.show()
+
+
+
+    # mu = {size: [] for size in (30, 50, 100, 200, 300, 500)}
+    # sigma = {size: [] for size in (30, 50, 100, 200, 300, 500)}
+    # for _ in tqdm(range(30)):
+    #     for size in (30, 50, 100, 200, 300, 500):
+    #         ep.set_portfolio(30, 250)
+    #         ep.optimize(price_fixed=True)
+    #         mu[size].append(np.mean(ep.results['losses']))
+    #         sigma[size].append((np.var(ep.results['losses']))**(1/2))
+
+    # for k, row in zip([10, 100, 200, 500, 1000], range(5)):
+    #     print(k)
+    #     ep.set_portfolio(k, 200)
+    #     x = np.asarray(ep._portfolios)
+    #     n, bins, patches = ax[row].hist(x.flatten(), **kwargs)
+    #     ax[row].yaxis.set_major_formatter(PercentFormatter(xmax=len(x.flatten() / n.max())))
+    #     ax[row].legend([k])
+    #     ax[row].grid(True)
+    #     ax[row].set_ylim([0, len(x.flatten()) * 0.20])
+    #
+    # plt.show()
     # r1 = ep.optimize(price_fixed=False)
     #
     # plt.hist(ep.results['losses'], bins=30)
